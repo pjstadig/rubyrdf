ff5aec29004b5e3d03cd7074d90e7282f4ade586
Paul Stadig <paul@stadig.name>
2008-07-11 07:03:57 -0400

    Added escaping to N-Triples output for URINode

--- test/rdf/uri_node_test.rb
+++ test/rdf/uri_node_test.rb
@@ -8,6 +8,8 @@ class RDF::URINodeTest < Test::Unit::TestCase
   def test_should_export_to_ntriples_format
     assert_equal '<http://stadig.name/>', RDF::URINode.new('http://stadig.name/').to_ntriples
     assert_equal '<http://stadig.name/>', RDF::URINode.new('http://stadig.name/').to_s
+    assert_equal '<http://stadig.name/\u0008\U00010000\t\n\r\"\\\\>',
+                 RDF::URINode.new('http://stadig.name/' + [0x8, 0x10000, 0x9, 0xa, 0xd, 0x22, 0x5c].pack('U*')).to_ntriples
   end
   
   def test_should_be_eq
