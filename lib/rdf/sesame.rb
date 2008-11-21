8e037e888b0ee822058f57125453faf7cd1c7919
Paul Stadig <paul@stadig.name>
2008-11-21 08:10:53 -0500

    Partial commit of sesame transaction support

--- lib/rdf/sesame.rb
+++ lib/rdf/sesame.rb
@@ -3,8 +3,185 @@ require 'net/http'
 
 module RDF
   class Sesame
-    def self.new(*a, &b)
-      Version2.new(*a, &b)
+    attr_reader :address, :port, :path, :repository
+    
+    def initialize(uri, repository)
+      uri = URI.parse(uri)
+      @address = uri.host
+      @port = uri.port
+      @path = uri.path
+      @repository = repository
+      @transactions = []
+    end
+    
+    def size
+      get_request(repo_path('size')).to_i
+    end
+    
+    def empty?
+      size == 0
+    end
+    
+    def add(*statement)
+      if @transactions.any?
+        @transactions.last << [:add, statement.to_statement]
+      else
+        post_request(repo_path('statements'), statement.to_statement.to_ntriples, {}, 'Content-Type' => 'text/plain')
+      end
+    end
+    
+    def import(data, format = :ntriples)
+      headers = case format
+      when :ntriples
+        {'Content-Type' => 'text/plain; charset=utf-8'}
+      when :rdfxml
+        {'Content-Type' => 'application/rdf+xml; charset=utf-8'}
+      end
+      
+      result = post_request(repo_path("statements"), data, {}, headers)
+      result
+    end
+    
+    def delete(*statement)
+      if @transactions.any?
+        @transactions.last << [:delete, statement.to_statement]
+      else
+        delete_request(repo_path('statements'), to_param_hash(statement.to_statement))
+      end
+    end
+    
+    def delete_all
+      if @transactions.any?
+        @transactions.last << [:delete_all, statement.to_statement]
+      else
+        delete_request(repo_path("statements"))
+      end
+    end
+    
+    def select(query)
+      SparqlResult.new(get_request(repo_path, {"query" => query}, "Accept" => "application/sparql-results+xml"))
+    end
+    
+    def ask(query)
+      get_request(repo_path, {"query" => query}, "Accept" => "text/boolean") == "true"
+    end
+    
+    def include?(*statement)
+      ask("ASK { #{statement.to_statement.to_ntriples} }")
+    end
+    
+    def transaction
+      @transactions << []
+      yield self
+      _commit(@transactions.pop)
+    rescue => e
+      @transactions.pop
+      throw e
+    end
+    
+    def rollback
+      @transactions.pop
+      @transactions << []
+      true
+    end
+    
+    def commit
+      _commit(@transactions.pop)
+      @transactions << []
+      true
+    end
+    
+    private
+    def repo_path(path = nil)
+      parts = [@path, "repositories", @repository]
+      if path.to_s.strip != ""
+        parts << path
+      end
+      
+      File.join(*parts)
+    end
+    
+    def to_param_hash(statement)
+      {:subj => statement.subject.to_ntriples,
+       :pred => statement.predicate.to_ntriples,
+       :obj => statement.object.to_ntriples}
+    end
+    
+    def get_request(path, params = {}, headers = {})
+      Net::HTTP.start(@address, @port) do |http|
+        http.get(
+          format_uri(path, params),
+          headers).body
+      end
+    end
+    
+    def post_request(path, data, params = {}, headers = {})
+      Net::HTTP.start(@address, @port) do |http|
+        http.open_timeout = 6000
+        http.read_timeout = 6000
+        http.post(format_uri(path, params), data, headers).body
+      end
+    end
+    
+    def delete_request(path, params = {}, headers = {})
+      Net::HTTP.start(@address, @port) do |http|
+        http.delete(format_uri(path, params), headers).body
+      end
+    end
+    
+    def to_transaction_xml(transaction)
+      b = Builder::XmlMarkup.new
+      b.transaction do
+        transaction.each do |op|
+          if op[0] == :add
+            b.add do
+              transaction_xml_nodes(b, op[1])
+            end
+          elsif op[0] == :delete
+            b.delete do
+              transaction_xml_nodes(b, op[1])
+            end
+          elsif op[0] == :delete_all
+            b.clear
+          end
+        end
+      end
+      b.target!
+    end
+    
+    def transaction_xml_nodes(b, stmt)
+      transaction_xml_node(b, stmt.subject)
+      transaction_xml_node(b, stmt.predicate)
+      transaction_xml_node(b, stmt.object)
+    end
+    
+    def transaction_xml_node(b, node)
+      if node.is_a?(URINode)
+        b.uri(node.uri)
+      elsif node.is_a?(BlankNode)
+        b.bnode(node.name)
+      elsif node.is_a?(TypedLiteralNode)
+        b.literal(node.lexical_form, :datatype => node.datatype_uri)
+      elsif node.is_a?(PlainLiteralNode)
+        attrs = {}
+        if node.language_tag
+          attrs[:"xml:lang"] = node.langage_tag
+        end
+        
+        b.literal(node.lexical_form, attrs)
+      end
+    end
+    
+    def _commit(transaction)
+      post_request(repo_path('statements'), to_transaction_xml(transaction), {}, 'Content-Type' => 'application/x-rdftransaction')
+    end
+    
+    def format_uri(path, params = {})
+      if params.empty?
+        path
+      else
+        path + "?" + params.map{|k, v| "#{k}=#{URI.escape(v)}"}.join("&")
+      end
     end
   end
 end
--- test/rdf/sesame_test.rb
+++ test/rdf/sesame_test.rb
@@ -1,7 +1,144 @@
 require File.dirname(__FILE__) + '/../test_helper.rb'
 
 class RDF::SesameTest < Test::Unit::TestCase
-  def test_default
-    assert true
+  def setup
+    @graph = RDF::Sesame.new('http://localhost:8180/openrdf-sesame', 'test')
+    @graph.delete_all
+    RDF::unregister_all!
+    RDF::register(:ex => 'http://example.org/')
+  end
+  
+  def test_should_initialize_address_port_path_and_repository
+    assert_equal 'localhost', @graph.address
+    assert_equal 8180, @graph.port
+    assert_equal '/openrdf-sesame', @graph.path
+    assert_equal 'test', @graph.repository
+  end
+  
+  def test_should_be_empty
+    assert @graph.empty?
+  end
+  
+  def test_should_add_statement
+    @graph.add(RDF[:ex]::sub, RDF[:ex]::pred, RDF[:ex]::obj)
+    assert_equal 1, @graph.size
+  end
+  
+  def test_should_import_data
+    @graph.import(<<-ENDL, :ntriples)
+      #{[RDF[:ex]::a, RDF[:ex]::b, RDF[:ex]::c].to_statement.to_ntriples}
+      #{[RDF[:ex]::d, RDF[:ex]::b, RDF[:ex]::e].to_statement.to_ntriples}
+    ENDL
+    assert_equal 2, @graph.size
+    assert @graph.include?([RDF[:ex]::a, RDF[:ex]::b, RDF[:ex]::c])
+    assert @graph.include?([RDF[:ex]::d, RDF[:ex]::b, RDF[:ex]::e])
+  end
+  
+  def test_should_delete_statement
+    @graph.add(RDF[:ex]::sub, RDF[:ex]::pred, RDF[:ex]::obj)
+    @graph.delete(RDF[:ex]::sub, RDF[:ex]::pred, RDF[:ex]::obj)
+    assert @graph.empty?
+  end
+  
+  def test_should_execute_select_query
+    assert @graph.select("SELECT ?x WHERE {?x #{RDF[:ex]::nothing} _:bn2 . }").empty?
+    
+    @graph.add(RDF[:ex]::a, RDF[:ex]::b, RDF::PlainLiteralNode.new('test'))
+    @graph.add(RDF[:ex]::d, RDF[:ex]::b, RDF::PlainLiteralNode.new('test', 'en'))
+    @graph.add(RDF[:ex]::d, RDF[:ex]::b, RDF::TypedLiteralNode.new('test', RDF[:ex]::a.uri))
+    @graph.add(RDF[:ex]::d, RDF[:ex]::b, RDF[:ex]::e)
+    @graph.add(RDF[:ex]::d, RDF[:ex]::b, RDF::BlankNode.new)
+    result = @graph.select("SELECT ?y WHERE {?x #{RDF[:ex]::b} ?y . }")
+    assert_equal 5, result.size
+    assert result.any?{|r| r['y'] == RDF::PlainLiteralNode.new('test')}
+    assert result.any?{|r| r['y'] == RDF::PlainLiteralNode.new('test', 'en')}
+    assert result.any?{|r| r['y'] == RDF::TypedLiteralNode.new('test', RDF[:ex]::a.uri)}
+    assert result.any?{|r| r['y'] == RDF[:ex]::e}
+    assert result.any?{|r| r['y'].blank_node?}
+  end
+  
+  def test_should_execute_ask_query
+    @graph.add(RDF[:ex]::sub, RDF[:ex]::pred, RDF[:ex]::obj)
+    assert @graph.ask("ask { #{[RDF[:ex]::sub, RDF[:ex]::pred, RDF[:ex]::obj].to_statement.to_ntriples} }")
+  end
+  
+  def test_should_include_statement
+    @graph.add(RDF[:ex]::sub, RDF[:ex]::pred, RDF[:ex]::obj)
+    assert @graph.include?(RDF[:ex]::sub, RDF[:ex]::pred, RDF[:ex]::obj)
+  end
+  
+  def test_should_clear_graph
+    @graph.add(RDF[:ex]::sub, RDF[:ex]::pred, RDF[:ex]::obj)
+    @graph.delete_all
+    
+    assert @graph.empty?
+  end
+  
+  def test_should_commit_transaction
+    @graph.add(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    @graph.transaction do |t|
+      t.add(RDF[:ex]::sub2, RDF[:ex]::pred2, RDF[:ex]::obj2)
+      t.delete(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    end
+    
+    assert !@graph.include?(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    assert @graph.include?(RDF[:ex]::sub2, RDF[:ex]::pred2, RDF[:ex]::obj2)
+  end
+  
+  def test_should_commit_in_middle_of_transaction
+    @graph.add(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    @graph.transaction do |t|
+      t.add(RDF[:ex]::sub2, RDF[:ex]::pred2, RDF[:ex]::obj2)
+      t.commit
+      t.delete(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    end
+    
+    assert !@graph.include?(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    assert @graph.include?(RDF[:ex]::sub2, RDF[:ex]::pred2, RDF[:ex]::obj2)
+  end
+  
+  def test_should_accept_empty_transaction
+    @graph.transaction do |t|
+    end
+  end
+  
+  def test_should_rollback_transaction
+    @graph.add(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    @graph.transaction do |t|
+      t.add(RDF[:ex]::sub2, RDF[:ex]::pred2, RDF[:ex]::obj2)
+      t.delete(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+      t.rollback
+    end
+    
+    assert @graph.include?(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    assert !@graph.include?(RDF[:ex]::sub2, RDF[:ex]::pred2, RDF[:ex]::obj2)
+  end
+  
+  def test_should_rollback_transaction_on_exception
+    @graph.add(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    begin
+      @graph.transaction do |t|
+        t.add(RDF[:ex]::sub2, RDF[:ex]::pred2, RDF[:ex]::obj2)
+        t.delete(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+        throw "test"
+      end
+      fail "Should rethrow exception"
+    rescue
+    end
+    
+    assert @graph.include?(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    assert !@graph.include?(RDF[:ex]::sub2, RDF[:ex]::pred2, RDF[:ex]::obj2)
+  end
+  
+  def test_should_rollback_in_middle_of_transaction
+    @graph.add(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    @graph.transaction do |t|
+      t.add(RDF[:ex]::sub2, RDF[:ex]::pred2, RDF[:ex]::obj2)
+      t.rollback
+      t.delete(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    end
+    
+    assert !@graph.include?(RDF[:ex]::sub1, RDF[:ex]::pred1, RDF[:ex]::obj1)
+    assert !@graph.include?(RDF[:ex]::sub2, RDF[:ex]::pred2, RDF[:ex]::obj2)
   end
 end
