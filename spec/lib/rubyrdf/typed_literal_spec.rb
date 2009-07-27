d5e6514e86dcb86135b9f298a9efab3425449ecc
Paul Stadig <paul@stadig.name>
2009-07-27 17:35:10 -0400

    Merged typed literal test into spec

--- spec/lib/rubyrdf/typed_literal_spec.rb
+++ spec/lib/rubyrdf/typed_literal_spec.rb
@@ -3,48 +3,87 @@ require File.expand_path(File.join(File.dirname(__FILE__), %w[.. .. spec_helper]
 describe RubyRDF::TypedLiteral do
   before do
     @lexical_form = "example"
-    @datatype_uri = "http://example.com/"
+    @datatype_uri = Addressable::URI.parse("http://example.com/")
     @it = RubyRDF::TypedLiteral.new(@lexical_form, @datatype_uri)
   end
 
-  it "should equal with same lexical_form and datatype_uri" do
-    @it.should == RubyRDF::TypedLiteral.new(@lexical_form, @datatype_uri)
+  describe "initialize" do
+    it "should assign lexical_form" do
+      @it.lexical_form.should == @lexical_form
+    end
+
+    it "should normalize lexical_form" do
+      RubyRDF::TypedLiteral.new([0x2126].pack('U'), @datatype_uri).lexical_form.should == [0x03a9].pack('U')
+    end
+
+    it "should assign datatype_uri" do
+      @it.datatype_uri.should == @datatype_uri
+    end
+
+    it "should normalize datatype_uri" do
+      RubyRDF::TypedLiteral.new(@lexical_form,
+                                Addressable::URI.parse("http://" + [0x2126].pack('U'))).
+        datatype_uri.to_s.should == Addressable::URI.parse("http://" + [0x03a9].pack('U'))
+    end
+
+    it "should normalize datatype_uri from a string" do
+      RubyRDF::TypedLiteral.new(@lexical_form, "http://" + [0x2126].pack('U')).
+        datatype_uri.to_s.should == Addressable::URI.parse("http://" + [0x03a9].pack('U'))
+    end
+
+    it "should convert datatype_uri from a string to an URI" do
+      RubyRDF::TypedLiteral.new(@lexical_form, @datatype_uri.to_s).should == @datatype_uri
+    end
   end
 
-  it "should not equal with different lexical_form and same datatype_uri" do
-    @it.should_not == RubyRDF::TypedLiteral.new("different", @datatype_uri)
-  end
+  describe "==" do
+    it "should equal with same lexical_form and datatype_uri" do
+      @it.should == RubyRDF::TypedLiteral.new(@lexical_form, @datatype_uri)
+    end
 
-  it "should not equal with same lexical_form and different datatype_uri" do
-    @it.should_not == RubyRDF::TypedLiteral.new(@lexical_form, "http://different.com/")
-  end
+    it "should not equal with different lexical_form and same datatype_uri" do
+      @it.should_not == RubyRDF::TypedLiteral.new("different", @datatype_uri)
+    end
 
-  it "should not equal nil" do
-    @it.should_not == nil
-  end
+    it "should not equal with same lexical_form and different datatype_uri" do
+      @it.should_not == RubyRDF::TypedLiteral.new(@lexical_form, "http://different.com/")
+    end
 
-  it "should not equal non-TypedLiteral" do
-    @it.should_not == Object.new
+    it "should not equal nil" do
+      @it.should_not == nil
+    end
+
+    it "should not equal non-TypedLiteral" do
+      @it.should_not == Object.new
+    end
   end
 
   it "should alias #== as #eql?" do
     @it.class.instance_method(:eql?).should == @it.class.instance_method(:==)
   end
 
-  it "should hash equal with same lexical_form and datatype_uri" do
-    @it.hash.should == RubyRDF::TypedLiteral.new(@lexical_form, @datatype_uri).hash
-  end
+  describe "hash" do
+    it "should be equal with same lexical_form and datatype_uri" do
+      @it.hash.should == RubyRDF::TypedLiteral.new(@lexical_form, @datatype_uri).hash
+    end
 
-  it "should not hash equal with different lexical_form and same datatype_uri" do
-    @it.hash.should_not == RubyRDF::TypedLiteral.new("different", @datatype_uri).hash
-  end
+    it "should not be equal with different lexical_form and same datatype_uri" do
+      @it.hash.should_not == RubyRDF::TypedLiteral.new("different", @datatype_uri).hash
+    end
 
-  it "should not hash equal with same lexical_form and different datatype_uri" do
-    @it.hash.should_not == RubyRDF::TypedLiteral.new(@lexical_form, "http://different.com/").hash
+    it "should not be equal with same lexical_form and different datatype_uri" do
+      @it.hash.should_not == RubyRDF::TypedLiteral.new(@lexical_form, "http://different.com/").hash
+    end
   end
 
   it "should return self for to_literal" do
     @it.to_literal.should equal(@it)
+  end
+
+  it "should be node" do
+    @it.should be_typed_literal_node
+    @it.should be_literal_node
+    @it.should be_node
   end
 end
 
