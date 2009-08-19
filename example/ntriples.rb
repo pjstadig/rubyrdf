a942831e34ae1424181f24d1155d533a61cac53f
Paul Stadig <paul@stadig.name>
2009-08-18 22:36:25 -0400

    Fixed path

--- example/ntriples.rb
+++ example/ntriples.rb
@@ -1,6 +1,6 @@
 $: << 'lib'
 require 'rubyrdf'
 
-File.open("spec/fixtures/Manifest.nt", "r") do |f|
+File.open("spec/fixtures/w3c/Manifest.nt", "r") do |f|
   RubyRDF::NTriples::Reader.new(f).each{|s|}
 end
